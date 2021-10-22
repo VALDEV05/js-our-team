45d8d2f7634c754c6fe17903e5554f19aa246957
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-22 16:10:11 +0200

    Create the folder structure, assign and process the delivery.

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+/* Descizione
+Il nostro compito é quello di rappresentare, utilizzando la struttura dati piú appropriata, una lista di membri di un team.
+Ciascun membro del team avrá come caratteristica name, role, image
+Per le immagini potete mettere il percorso di un immagine presa da internet, provate con lorem pixum (https://picsum.photos/images)
+Stampare in console tutti gli elementi del team e le loro proprieta.
+Esempio
+nome: Ben Green
+role: CEO
+image: https://picsum.photos/id/1025/300/300
+Consigli:
+Pensate alla struttura dati che volete usare, cercando di porvi alcune domande:
+In quale struttura dati salverò i miei dati?
+a. Me ne serve più di una?
+b. Quale è la scelta migliore in termini di complessità?
+Ci sono dei cicli?
+a. La scelta dei cicli è coerente con le strutture dati che ho scelto?
+b. Potrebbe esserci una via più semplice se cambiassi struttura dati? */
+
+
+
+/* Obiettivo principale */
+/* Il nostro compito é quello di rappresentare, utilizzando la struttura dati piú appropriata, 
+una lista di membri di un team. */
+
+
+// Ciascun membro del team avrá come caratteristica name, role, image (https://picsum.photos/images)
+//stampare tutto in console 
+//esempio Esempio
+/*  nome: Ben Green
+    role: CEO
+    image: https://picsum.photos/id/1025/300/300 */
